aad13e352a9c5341b4cf73605add17ff93f543b4
BugHunter-X <askzenai@gmail.com>
2025-02-15 06:28:10 +0100

    Add bug.js file

---
+++ bug.js
@@ -0,0 +1,7 @@
+```javascript
+//This is a javascript file that uses tailwindcss
+/*This is a tailwindcss class that has an error*/
+.bg-gradient-to-r-from-blue-500-to-red-500 {
+  background-image: linear-gradient(to right, rgb(59, 130, 246), rgb(239, 68, 68));
+}
+```
